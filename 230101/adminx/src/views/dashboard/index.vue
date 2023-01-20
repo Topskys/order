4d7b23848df58bf650ae3187f4e2048e05b46696
Topskys <2477350343@qq.com>
2023-01-20 23:29:04 +0800

    update *

---
+++ 230101/adminx/src/views/dashboard/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <countTo :startVal="startVal" :endVal="endVal" :duration="3000"></countTo>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import countTo from "vue-count-to";
+export default {
+  name: "Dashboard",
+  components: { countTo },
+  data() {
+    return {
+      startVal: 0,
+      endVal: 2023,
+    };
+  },
+  computed: {
+    ...mapGetters(["name"]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
